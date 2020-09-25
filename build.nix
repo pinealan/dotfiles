62a4b04fbf743a592d7f8b285b0e89c8dff58d69
Alan Chan <alan.c@gini.co>
2020-09-25 19:01:33 +0800

    Make build.nix work

--- build.nix
+++ build.nix
@@ -1,17 +1,23 @@
 with import <nixpkgs> {};
-{
-  buildInputs = [
-    neovim
-    python37.withPackages (ps: with ps; [ pynvim ])
-    # Sysadmin
-    htop stow lolcat
-    # CLI data view / search / processing
-    bat fd fzf ripgrep miller jq yq icdiff
-    # Dev environment
-    cloc direnv entr modd devd
-    # Benchmarking
-    wrk
-    # Language
-    clojure go rust nodejs yarn
-  ]
+
+mkShell {
+    buildInputs = [
+        neovim
+        (python39.withPackages (ps: with ps; [ virtualenv setuptools wheel ]))
+
+        # Sysadmin
+        htop stow lolcat
+
+        # CLI data view / search / processing
+        bat fd fzf ripgrep miller jq yq icdiff
+
+        # Dev environment
+        cloc direnv entr modd devd
+
+        # Benchmarking
+        wrk
+
+        # Language
+        clojure go rust nodejs yarn
+    ];
 }
