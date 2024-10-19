3a4f9c339038bedb92eecaab60e5872a2c46be04
Alan Chan <achan961117@gmail.com>
2024-10-19 02:32:25 +0200

    Treesitter config

--- nvim/.config/nvim/lua/pinealan.lua
+++ nvim/.config/nvim/lua/pinealan.lua
@@ -61,8 +61,8 @@ require('cmp').setup({
 
 --[[ Treesitter ]]
 
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { "c", "lua", "python", "vim" },
+require('nvim-treesitter.configs').setup({
+  ensure_installed = { "c", "lua", "python", "rust", "vim" },
 
   sync_install = false,
   auto_install = false,
@@ -71,29 +71,10 @@ require('nvim-treesitter.configs').setup {
   ignore_install = { "javascript" },
 
   highlight = {
-    enable = false,
+    enable = true,
     additional_vim_regex_highlighting = false,
   },
-
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25,
-    persist_queries = false,
-    keybindings = {
-      toggle_query_editor = 'o',
-      toggle_hl_groups = 'i',
-      toggle_injected_languages = 't',
-      toggle_anonymous_nodes = 'a',
-      toggle_language_display = 'I',
-      focus_language = 'f',
-      unfocus_language = 'F',
-      update = 'R',
-      goto_node = '<cr>',
-      show_help = '?',
-    },
-  }
-}
+})
 
 --[[ Others ]]
 
