16e5ea8422c46d7b2e37a7c5af858508472c20de
Alan Chan <achan961117@gmail.com>
2025-04-09 00:01:24 +0200

    nvim: Disable treesitter hl on clojure

--- nvim/.config/nvim/plugin/pinealan.lua
+++ nvim/.config/nvim/plugin/pinealan.lua
@@ -129,6 +129,9 @@ require('nvim-treesitter.configs').setup({
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
+    disable = {
+        'clojure',
+    }
   },
 })
 
