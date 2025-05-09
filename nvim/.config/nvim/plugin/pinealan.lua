cc7b3af4366cc8da76ab545a11d5b84acf9b80c2
Alan Chan <achan961117@gmail.com>
2025-05-09 13:23:16 +0200

    nvim: neovide maps for faster rescale

--- nvim/.config/nvim/plugin/pinealan.lua
+++ nvim/.config/nvim/plugin/pinealan.lua
@@ -251,6 +251,10 @@ if vim.g.neovide then
     local change_scale_factor = function(delta)
       vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
     end
+
     vim.keymap.set("n", "<D-=>", function() change_scale_factor(1.05) end)
     vim.keymap.set("n", "<D-->", function() change_scale_factor(1/1.05) end)
+
+    vim.keymap.set("n", "<D-+>", function() change_scale_factor(1.1025) end)
+    vim.keymap.set("n", "<D-_>", function() change_scale_factor(1/1.1025) end)
 end
