8886b1828920245f161cfbdc348cc4abf898c168
Alan Chan <achan961117@gmail.com>
2025-07-08 20:35:11 +0200

    nvim: Ignore image files in repo search

--- nvim/.config/nvim/lua/pinealan.lua
+++ nvim/.config/nvim/lua/pinealan.lua
@@ -5,8 +5,6 @@ local M = {}
 local is_inside_work_tree = {}
 
 M.project_files = function()
-  local opts = {} -- define here if you want to define something
-
   local cwd = vim.fn.getcwd()
   if is_inside_work_tree[cwd] == nil then
     vim.fn.system("git rev-parse --is-inside-work-tree")
@@ -14,9 +12,15 @@ M.project_files = function()
   end
 
   if is_inside_work_tree[cwd] then
-    require("telescope.builtin").git_files(opts)
+    require("telescope.builtin").git_files({
+        show_untracked = true,
+        git_command = {
+            "git","ls-files","--exclude-standard","--cached",
+            "--",".", ":!:*.png", ":!:*.jpg", ":!:*.jpeg"
+        },
+    })
   else
-    require("telescope.builtin").find_files(opts)
+    require("telescope.builtin").find_files()
   end
 end
 
